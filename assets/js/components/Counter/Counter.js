d3ed066a7d967687066fed037dccf334aba8b103
nufftalon <hawktimewebdesign@gmail.com>
2021-04-29 00:15:23 -0400

    added styling for counter elements using embedded css on component

--- assets/js/components/Counter/Counter.js
+++ assets/js/components/Counter/Counter.js
@@ -7,6 +7,48 @@ export default class Counter extends Component {
 	}
 
 	render() {
-		return <div id="counter-comp">This is a counter</div>;
+		return (
+			<div id="counter-comp" style={styleCounterComp}>
+				<div className="number" style={styleNumber}>
+					0
+				</div>
+				<div className="buttons" style={styleButtons}>
+					<div className="button minus" style={styleButton}>
+						-
+					</div>
+					<div className="button plus" style={styleButton}>
+						+
+					</div>
+				</div>
+			</div>
+		);
 	}
 }
+
+const styleCounterComp = {
+	width: '100%',
+	maxWidth: '400px',
+	margin: '0 auto'
+};
+
+const styleNumber = {
+	border: '3px solid black',
+	padding: '20px',
+	fontSize: '2rem',
+	fontWeight: '900',
+	textAlign: 'center'
+};
+
+const styleButton = {
+	width: '50%',
+	border: '3px solid black',
+	padding: '20px',
+	fontSize: '2rem',
+	fontWeight: '900',
+	textAlign: 'center',
+	cursor: 'pointer'
+};
+
+const styleButtons = {
+	display: 'flex'
+};
