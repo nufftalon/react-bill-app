22798873179fffd033018441d9886893a712b5d1
nufftalon <hawktimewebdesign@gmail.com>
2021-04-29 22:38:06 -0400

    changed order of array

--- assets/js/components/Immutable/Immutable.js
+++ assets/js/components/Immutable/Immutable.js
@@ -5,6 +5,9 @@ export default class Immutable extends Component {
 		super();
 		this.state = {
 			background: 'black',
+			names: ['Cindy', 'Billy', 'Lisa', 'John'],
+			both: [],
+			teachers: ['Cam', 'Brad', 'Jennifer'],
 			user: {
 				name: 'Gary',
 				age: 23,
@@ -20,28 +23,33 @@ export default class Immutable extends Component {
 	}
 	clickedBtn = () => {
 		console.log(this.state);
-		const newGrade = {
-			...this.state.user.grades,
-			math: 'A+'
+		// Add name
+		// const newState = {
+		// 	names: [...this.state.names, 'Ramsey']
+		// };
+
+		//Remove
+		// const newState = {
+		// 	names: this.state.names.filter(name => name !== 'Billy')
+		// };
+
+		//Merge two arrays
+		// const newState = {
+		// 	both: [...this.state.names, ...this.state.teachers]
+		// };
+
+		//sort array
+		const newState = {
+			names: this.state.names.slice().sort()
 		};
 
-		const newUser = Object.assign({}, this.state.user, {
-			name: 'Mark',
-			grades: newGrade
+		this.setState(newState, () => {
+			console.log(this.state);
 		});
-		this.setState(
-			{
-				background: 'green',
-				user: newUser
-			},
-			() => {
-				console.log(this.state);
-			}
-		);
 		console.log('Clicked BTN');
 	};
 	changeToActive = () => {
-		if (this.state.user.grades.math == 'A+') {
+		if (this.state.names[0] == 'Billy') {
 			return 'active';
 		} else {
 			return '';
