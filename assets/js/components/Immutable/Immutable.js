7a9ace5b0bc4ae7959be9fdbd2fb437de658de4c
nufftalon <hawktimewebdesign@gmail.com>
2021-04-29 20:56:51 -0400

    change math state of button click

--- assets/js/components/Immutable/Immutable.js
+++ assets/js/components/Immutable/Immutable.js
@@ -4,17 +4,42 @@ export default class Immutable extends Component {
 	constructor() {
 		super();
 		this.state = {
-			background: 'black'
+			background: 'black',
+			user: {
+				name: 'Gary',
+				age: 23,
+				location: 'NYC',
+				grades: {
+					math: 'A',
+					english: 'B+',
+					science: 'C-',
+					gym: 'F'
+				}
+			}
 		};
 	}
 	clickedBtn = () => {
-		this.setState({
-			background: 'green'
+		console.log(this.state);
+		const newGrade = Object.assign({}, this.state.user.grades, {
+			math: 'A+'
 		});
+		const newUser = Object.assign({}, this.state.user, {
+			name: 'Mark',
+			grades: newGrade
+		});
+		this.setState(
+			{
+				background: 'green',
+				user: newUser
+			},
+			() => {
+				console.log(this.state);
+			}
+		);
 		console.log('Clicked BTN');
 	};
 	changeToActive = () => {
-		if (this.state.background == 'green') {
+		if (this.state.user.grades.math == 'A+') {
 			return 'active';
 		} else {
 			return '';
