8ffc7ae719956c59600218f60e3a41cc60e8d1e7
nufftalon <hawktimewebdesign@gmail.com>
2021-05-03 11:24:17 -0400

    created BillsApp.js

---
+++ assets/js/components/BillsApp/BillsApp.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import update from 'immutability-helper';
+
+export default class BillsApp extends Component {
+	constructor() {
+		super();
+		this.state = {
+		
+		};
+	}
+	
+
+
+	render() {
+		return (
+		<div className="phone-container"></div>
+		);
+	}
+}
